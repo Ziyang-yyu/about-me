cfa0f444c8066171f0dfd44493091fc02559eac7
h28219zy <ziyang.yu@student.ac.uk>
2021-10-03 02:20:19 +0800

    Remove futile queries

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 'use strict';
 
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Button scrolling
